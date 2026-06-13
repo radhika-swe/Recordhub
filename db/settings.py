70b64ee9665d17f9c214164519da5a917ab5845a
RADHIKA SHARMA <edu112radhika@gmail.com>
2026-06-13 13:26:46 +0530

    fix middleware ellipsis crash

--- db/settings.py
+++ db/settings.py
@@ -51,7 +51,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    ...
 ]
 
 ROOT_URLCONF = 'db.urls'
